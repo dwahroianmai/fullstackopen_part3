bf0a261d06184e059b460c832913699879bda314
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-07-02 15:47:06 +0200

    exercise 3.13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 const app = express();
 
 app.use(express.static("build"));
@@ -13,39 +15,21 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 // get the list of all people in the phonebook
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({})
+    .then((people) => response.json(people))
+    .catch((error) => console.log(error));
 });
 
 // get one person from the phonebook
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-  person ? response.json(person) : response.status(404).end();
+  Person.findById(request.params.id)
+    .then((person) => response.json(person))
+    .catch((error) => {
+      console.log(error);
+      response.status(404).end();
+    });
 });
 
 // delete the person from the phonebook
@@ -88,5 +72,5 @@ app.get("/info", (request, response) => {
   );
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
---
+++ models/person.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+mongoose
+  .connect(url)
+  .then(() => console.log("connected to mongodb"))
+  .catch((error) => console.log(error.message));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
